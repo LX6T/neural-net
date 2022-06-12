d952db2edf20a468c109b3fec946499f88684c38
alex <alex@pullen.net.nz>
2022-06-12 20:21:43 +1200

    Implemented some alternative constructors, overloaded arithmetic operators including "*" for the dot product of two matrices

--- src/matrix.h
+++ src/matrix.h
@@ -7,8 +7,11 @@
 template <typename T>
 class matrix {
 public:
-    // Declare constructor
+    // Declare constructors
+    matrix();
+    matrix(int rows, int cols);
     matrix(int rows, int cols, const T* inputData);
+    matrix(const matrix<T>& rhs);
 
     // Declare destructor
     ~matrix();
@@ -42,11 +45,36 @@ private:
     int subToInd(int row, int col);     // Converts (rows, columns) to (index)
 };
 
-// Define constructor
+
+// CONSTRUCTOR (default)
 template<typename T>
-matrix<T>::matrix(int rows, int cols, const T* inputData) {
-    this->rows = rows;
-    this->cols = cols;
+matrix<T>::matrix() {
+    rows = 1;
+    cols = 1;
+    elements = rows * cols;
+    matrixData = new T[elements];
+    for (int i=0; i<elements; ++i) {
+        matrixData[i] = 0;
+    }
+}
+
+// CONSTRUCTOR (zeros)
+template<typename T>
+matrix<T>::matrix(int nRows, int nCols) {
+    rows = nRows;
+    cols = nCols;
+    elements = rows * cols;
+    matrixData = new T[elements];
+    for (int i=0; i<elements; ++i) {
+        matrixData[i] = 0;
+    }
+}
+
+// CONSTRUCTOR (input)
+template<typename T>
+matrix<T>::matrix(int nRows, int nCols, const T* inputData) {
+    rows = nRows;
+    cols = nCols;
     elements = rows * cols;
     matrixData = new T[elements];
     for (int i=0; i<elements; ++i) {
@@ -54,13 +82,26 @@ matrix<T>::matrix(int rows, int cols, const T* inputData) {
     }
 }
 
-// Define destructor
+// CONSTRUCTOR (copy)
+template<typename T>
+matrix<T>::matrix(const matrix<T>& rhs) {
+    rows = rhs.getRows();
+    cols = rhs.getCols();
+    elements = rows * cols;
+    matrixData = new T[elements];
+    for (int i=0; i<elements; ++i) {
+        matrixData[i] = rhs.getElement(i);
+    }
+}
+
+// DESTRUCTOR
 template<typename T>
 matrix<T>::~matrix() {
     delete[] matrixData;
 }
 
-// Element Getter
+
+// GETTER (element)
 template<typename T>
 T matrix<T>::getElement(int row, int col) {
     int index = subToInd(row, col);
@@ -71,7 +112,7 @@ T matrix<T>::getElement(int row, int col) {
     }
 }
 
-// Element Setter
+// SETTER (element)
 template<typename T>
 bool matrix<T>::setElement(int row, int col, T value) {
     int index = subToInd(row, col);
@@ -83,13 +124,13 @@ bool matrix<T>::setElement(int row, int col, T value) {
     }
 }
 
-// Getter for number of rows
+// GETTER (#rows)
 template<typename T>
 int matrix<T>::getRows() {
     return rows;
 }
 
-// Getter for number of columns
+// GETTER (#columns)
 template<typename T>
 int matrix<T>::getCols() {
     return cols;
@@ -118,58 +159,159 @@ bool matrix<T>::operator==(const matrix<T> &rhs) {
 // matrix + matrix
 template<class U>
 matrix<U> operator+(const matrix<U> &lhs, const matrix<U> &rhs) {
-
-    return matrix<U>(0, 0);
+    int rows = lhs.rows;
+    int cols = lhs.cols;
+    int elements = lhs.elements;
+    U* tempSum = new U[elements];
+    for (int i = 0; i < elements; ++i) {
+        tempSum[i] = lhs.matrixData[i] + rhs.matrixData[i];
+    }
+    matrix<U> sum(rows, cols, tempSum);
+    delete[] tempSum;
+    return sum;
 }
 
 // scalar + matrix
 template<class U>
 matrix<U> operator+(const U &lhs, const matrix<U> &rhs) {
-    return matrix<U>(0, 0);
+    int rows = rhs.rows;
+    int cols = rhs.cols;
+    int elements = rhs.elements;
+    U* tempSum = new U[elements];
+    for (int i = 0; i < elements; ++i) {
+        tempSum[i] = lhs + rhs.matrixData[i];
+    }
+    matrix<U> sum(rows, cols, tempSum);
+    delete[] tempSum;
+    return sum;
 }
 
 // matrix + scalar
 template<class U>
 matrix<U> operator+(const matrix<U> &lhs, const U &rhs) {
-    return matrix<U>(0, 0);
+    int rows = lhs.rows;
+    int cols = lhs.cols;
+    int elements = lhs.elements;
+    U* tempSum = new U[elements];
+    for (int i = 0; i < elements; ++i) {
+        tempSum[i] = lhs.matrixData[i] + rhs;
+    }
+    matrix<U> sum(rows, cols, tempSum);
+    delete[] tempSum;
+    return sum;
 }
 
 
 // matrix - matrix
 template<class U>
 matrix<U> operator-(const matrix<U> &lhs, const matrix<U> &rhs) {
-    return matrix<U>(0, 0);
+    int rows = lhs.rows;
+    int cols = lhs.cols;
+    int elements = lhs.elements;
+    U* tempDiff = new U[elements];
+    for (int i = 0; i < elements; ++i) {
+        tempDiff[i] = lhs.matrixData[i] - rhs.matrixData[i];
+    }
+    matrix<U> diff(rows, cols, tempDiff);
+    delete[] tempDiff;
+    return diff;
 }
 
 // scalar - matrix
 template<class U>
 matrix<U> operator-(const U &lhs, const matrix<U> &rhs) {
-    return matrix<U>(0, 0);
+    int rows = rhs.rows;
+    int cols = rhs.cols;
+    int elements = rhs.elements;
+    U* tempDiff = new U[elements];
+    for (int i = 0; i < elements; ++i) {
+        tempDiff[i] = lhs - rhs.matrixData[i];
+    }
+    matrix<U> diff(rows, cols, tempDiff);
+    delete[] tempDiff;
+    return diff;
 }
 
 // matrix - scalar
 template<class U>
 matrix<U> operator-(const matrix<U> &lhs, const U &rhs) {
-    return matrix<U>(0, 0);
+    int rows = lhs.rows;
+    int cols = lhs.cols;
+    int elements = lhs.elements;
+    U* tempDiff = new U[elements];
+    for (int i = 0; i < elements; ++i) {
+        tempDiff[i] = lhs.matrixData[i] - rhs;
+    }
+    matrix<U> diff(rows, cols, tempDiff);
+    delete[] tempDiff;
+    return diff;
 }
 
 
 // matrix * matrix
 template<class U>
 matrix<U> operator*(const matrix<U> &lhs, const matrix<U> &rhs) {
-    return matrix<U>(0, 0);
+    int leftRows = lhs.rows;
+    int leftCols = lhs.cols;
+    int rightRows = rhs.rows;
+    int rightCols = rhs.cols;
+
+    if (leftCols != rightRows) {
+        matrix<U> dotProduct(1, 1);
+        return dotProduct;
+    }
+
+    int prodRows = leftRows;
+    int prodCols = rightCols;
+    int prodElements = leftRows * rightCols;
+
+    U* tempProd = new U[prodElements];
+    for (int i = 0; i < leftRows; ++i) {
+        for (int j = 0; j < rightCols; ++j) {
+            int prodIndex = rhs.subToInd(prodRows, prodCols);
+            U sumOfProducts = 0.0;
+            for (int k = 0; k < leftCols; ++k) {
+                int leftIndex = lhs.subToInd(i, k);
+                int rightIndex = rhs.subToInd(k, j);
+                sumOfProducts += lhs.matrixData[leftIndex] * rhs.matrixData[rightIndex];
+            }
+            tempProd[prodIndex] = sumOfProducts;
+        }
+    }
+
+    matrix<U> dotProduct(prodRows, prodCols, tempProd);
+    delete[] tempProd;
+    return dotProduct;
 }
 
 // scalar * matrix
 template<class U>
 matrix<U> operator*(const U &lhs, const matrix<U> &rhs) {
-    return matrix<U>(0, 0);
+    int rows = rhs.rows;
+    int cols = rhs.cols;
+    int elements = rhs.elements;
+    U* tempProd = new U[elements];
+    for (int i = 0; i < elements; ++i) {
+        tempProd[i] = lhs - rhs.matrixData[i];
+    }
+    matrix<U> prod(rows, cols, tempProd);
+    delete[] tempProd;
+    return prod;
 }
 
 // matrix * scalar
 template<class U>
 matrix<U> operator*(const matrix<U> &lhs, const U &rhs) {
-    return matrix<U>(0, 0);
+    int rows = lhs.rows;
+    int cols = lhs.cols;
+    int elements = lhs.elements;
+    U* tempProd = new U[elements];
+    for (int i = 0; i < elements; ++i) {
+        tempProd[i] = lhs.matrixData[i] - rhs;
+    }
+    matrix<U> prod(rows, cols, tempProd);
+    delete[] tempProd;
+    return prod;
 }
 
 

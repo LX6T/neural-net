c5f9cb7ce025c45adddb70882f76ee87e9698c0e
LX6T <alex@pullen.net.nz>
2022-07-13 23:36:18 +1200

    Number of layers is now a parameter of trainTest() and predictTest()

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,16 @@
 #include "NeuralNet.h"
 
 
-void trainTest(const std::string& filename, int n) {
+void trainTest(const std::string& filename, int n, int L) {
     std::vector<Image> trainingImages = csvToImages(filename, n);
-    NeuralNet net = NeuralNet(3, 784, 300, 10);
+    NeuralNet net = NeuralNet(L, 784, 300, 10);
     net.train(trainingImages, n);
     net.save("../networks");
 }
 
-void predictTest(const std::string& filename, int n) {
+void predictTest(const std::string& filename, int n, int L) {
     std::vector<Image> predictingImages = csvToImages(filename, n);
-    NeuralNet net = NeuralNet(3, 784, 300, 10);
+    NeuralNet net = NeuralNet(L, 784, 300, 10);
     net.load("../networks");
     double accuracy = net.predictImages(predictingImages, n);
     std::cout << "Accuracy: " << accuracy << std::endl;
@@ -21,13 +21,8 @@ void predictTest(const std::string& filename, int n) {
 
 int main() {
 
-//    trainTest("../data/mnist_test.csv", 2500);
-//    predictTest("../data/mnist_test.csv", 10000);
-
-    for (int i = 500; i < 10000; i = i + 500) {
-        trainTest("../data/mnist_test.csv", i);
-        predictTest("../data/mnist_test.csv", 10000);
-    }
+    trainTest("../data/mnist_test.csv", 10000, 2);
+    predictTest("../data/mnist_test.csv", 10000, 2);
 
     return 0;
 }
